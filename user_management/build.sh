0661ce78aa17005cf72bceaa8fd67fa636aa2842
Amaya-python <amayasurendran148@gmail.com>
2026-09-01 16:15:21 +0530

    feat: add automatic superuser creation to build script

--- user_management/build.sh
+++ user_management/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+
+# Create superuser automatically if it doesn't exist
+python manage.py createsuperuser --no-input || true
