5e57e26285baa4cdf21accd862365552ce1ecf8d
Amaya-python <amayasurendran148@gmail.com>
2026-09-01 15:57:47 +0530

    fix: add psycopg2 dependency, admin registration, and build script

--- user_management/users/admin.py
+++ user_management/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'age', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email')
